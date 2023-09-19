3f9288705b91dacc45ccb10f38c413773999fed0
Park Chaerhin <127855706+park-chaerhin@users.noreply.github.com>
2023-09-19 22:56:12 +0900

    header

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-$(function (){
-    let origin = window.pageYOffset;
-    add_active(origin);
+// $(function (){
+//     let origin = window.pageYOffset;
+//     add_active(origin);
 
-    window.addEventListener('scroll', function(){
-        let scroll = window.pageXOffset;
-        add_active(scroll);
-    });
-});
+//     window.addEventListener('scroll', function(){
+//         let scroll = window.pageXOffset;
+//         add_active(scroll);
+//     });
+// });
 
-function add_active (){
-    if (scroll > 0){
-        $('header').addClass('active');
+// function add_active (){
+//     if (scroll > 0){
+//         $('header').addClass('active');
+//     } else {
+//         $('header').romoveClass('active');
+//     }
+// }
+
+
+window.addEventListener("scroll", function() {
+    var header = document.getElementById('header');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) {
+        header.style.backgroundColor = '#fff';
     } else {
-        $('header').romoveClass('active');
+        header.style.backgroundColor = 'unset'; // 초기 배경색
     }
-}
+});
